36ff8608790d62d76c9ba320322efb2ba9c15bc0
Md. Tazwarul Islam Abir <md.tazwarul.islam.07@gmail.com>
2024-06-12 10:36:30 +0600

    basic setup

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -4,6 +4,6 @@ const router = Router();
 
 const moduleRoutes = [{ path: '', route: '' }];
 
-moduleRoutes.forEach((route) => router.use(route.path, route.route));
+//moduleRoutes.forEach((route) => router.use(route.path, route.route));
 
 export default router;
